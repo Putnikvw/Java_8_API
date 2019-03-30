557c80a6ccf248944ac4835a23d6e1b8ce5d1ffe
Igor98 <igor.v.mou@gmail.com>
2019-03-30 21:57:57 +0200

    add new Test

--- test/com/voitov/java/api/JUnit_WriteMap.java
+++ test/com/voitov/java/api/JUnit_WriteMap.java
@@ -40,4 +40,14 @@ class JUnit_WriteMap {
         assertEquals(fullRacerName, file.getRacer().get(racerFam).getName());
     }
 
+    @Test
+    public void checkPrintStringForRacer() {
+
+        FileData file = new FileData();
+        Painter print = new Painter();
+        String firstRacer = print.racersString(file.getRacer()).substring(0, print.racersString(file.getRacer()).indexOf("\n"));
+        String racerInTop = "1. Esteban Ocon\t\t| FORCE INDIA MERCEDES\t| 54:13.028";
+        assertEquals(racerInTop, firstRacer);
+    }
+
 }
