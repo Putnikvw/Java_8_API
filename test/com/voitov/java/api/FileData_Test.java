9bbdf55d6be658fffee5fc9fc57393a5d5661adb
Igor98 <igor.v.mou@gmail.com>
2019-04-01 20:21:45 +0300

    add test and change name

--- test/com/voitov/java/api/FileData_Test.java
+++ test/com/voitov/java/api/FileData_Test.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class JUnit_WriteMap {
+class FileData_Test {
 
     @Test
     public void checkTimeForRacer() {
@@ -40,14 +40,6 @@ class JUnit_WriteMap {
         assertEquals(fullRacerName, file.getRacer().get(racerFam).getName());
     }
 
-    @Test
-    public void checkPrintStringForRacer() {
 
-        FileData file = new FileData();
-        Painter print = new Painter();
-        String firstRacer = print.racersString(file.getRacer()).substring(0, print.racersString(file.getRacer()).indexOf("\n"));
-        String racerInTop = "1. Esteban Ocon\t\t| FORCE INDIA MERCEDES\t| 54:13.028";
-        assertEquals(racerInTop, firstRacer);
-    }
 
 }
---
+++ test/com/voitov/java/api/Painter_Test.java
@@ -0,0 +1,16 @@
+package com.voitov.java.api;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Painter_Test {
+    @Test
+    public void checkPrintStringForRacer() {
+
+        FileData file = new FileData();
+        Painter print = new Painter();
+        String firstRacer = print.racersString(file.getRacer()).substring(0, print.racersString(file.getRacer()).indexOf("\n"));
+        String racerInTop = "1. Esteban Ocon\t\t| FORCE INDIA MERCEDES\t| 54:13.028";
+        assertEquals(racerInTop, firstRacer);
+    }
+}
