f63c9851a1ade278771baa5d5d704d1cdf860b11
Igor <igor.v.mou@gmail.com>
2019-04-04 18:07:22 +0300

    change test

--- test/com/voitov/java/api/DataTest.java
+++ test/com/voitov/java/api/DataTest.java
@@ -1,60 +1,55 @@
 package com.voitov.java.api;
 
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class DataTest {
 
+    @Mock
+    FileReader fileReader;
+
+    @InjectMocks
+    Data fileData;
 
     @Test
-    public void checkDataFromFileReader() {
+    public void checkTimeForRacer() {
 
-        FileReader fileData = Mockito.mock(FileReader.class);
-        Data data = Mockito.mock(Data.class);
-        Duration time = Duration.ofMillis(20);
-        List<Racer> racers = new ArrayList<>();
-        racers.add(new Racer("Alan", "Ferrari", time));
-        String filePath = "Test";
-        when(fileData.readFile(filePath)).thenReturn("EOF_Esteban Ocon_FORCE INDIA MERCEDES").thenReturn("EOF2018-05-24_12:17:58.810")
-                .thenReturn("EOF2018-05-24_12:12:11.838");
-        when(data.getRacer()).thenReturn(racers);
-        assertEquals(data.getRacer().get(0).getName(), racers.get(0).getName());
+        DateTimeFormatter timePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS");
+        LocalDateTime startLapTime = LocalDateTime.parse("2018-05-24_12:02:58.917", timePattern);
+        LocalDateTime finishLapTime = LocalDateTime.parse("2018-05-24_12:04:03.332", timePattern);
+        Duration resultTime = Duration.between(startLapTime, finishLapTime);
+        when(fileReader.readFile(anyString())).thenReturn("SVF_Sebastian Vettel_FERRARI").thenReturn("SVF2018-05-24_12:02:58.917")
+                .thenReturn("SVF2018-05-24_12:04:03.332");
+        assertEquals(fileData.getRacer().get(0).getTime(), resultTime);
     }
 
+    @Test
+    public void checkCarTitleForRacer() {
 
-//    @Test
-//    public void checkTimeForRacer() {
-//
-//        int racerSvf = 1;
-//        DateTimeFormatter timePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS");
-//        LocalDateTime startLapTime = LocalDateTime.parse("2018-05-24_12:02:58.917", timePattern);
-//        LocalDateTime finishLapTime = LocalDateTime.parse("2018-05-24_12:04:03.332", timePattern);
-//        Duration resultTime = Duration.between(startLapTime, finishLapTime);
-//        assertEquals(resultTime, fileData.getRacer().get(racerSvf).getTime());
-//    }
-//
-//    @Test
-//    public void checkCarTitleForRacer() {
-//
-//        int racerMes = 16;
-//        String fullCarTitle = "SAUBER FERRARI";
-//        assertEquals(fullCarTitle, fileData.getRacer().get(racerMes).getCar());
-//    }
-//
-//    @Test
-//    public void checkFullNameForRacer() {
-//
-//        int racerFam = 6;
-//        String fullRacerName = "Fernando Alonso";
-//        assertEquals(fullRacerName, fileData.getRacer().get(racerFam).getName());
-//    }
+        when(fileReader.readFile(anyString())).thenReturn("EOF_Esteban Ocon_FORCE INDIA MERCEDES").thenReturn("EOF2018-05-24_12:17:58.810")
+                .thenReturn("EOF2018-05-24_12:12:11.838");
+        assertEquals(fileData.getRacer().get(0).getCar(), "FORCE INDIA MERCEDES");
+    }
+
+    @Test
+    public void checkFullNameForRacer() {
+
+        when(fileReader.readFile(anyString())).thenReturn("EOF_Esteban Ocon_FORCE INDIA MERCEDES").thenReturn("EOF2018-05-24_12:17:58.810")
+                .thenReturn("EOF2018-05-24_12:12:11.838");
+        assertEquals(fileData.getRacer().get(0).getName(), "Esteban Ocon");
+    }
 
 
 }
--- test/com/voitov/java/api/PainterTest.java
+++ test/com/voitov/java/api/PainterTest.java
@@ -1,27 +1,31 @@
 package com.voitov.java.api;
 
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
 
-import static org.mockito.Mockito.*;
-
-
+@RunWith(MockitoJUnitRunner.class)
 public class PainterTest {
 
-    Data fileData = Mockito.mock(Data.class);
-    
+    @Mock
+    FileReader fileReader;
+
+    @InjectMocks
+    Data fileData;
+
     @Test
     public void checkPrintStringForRacer() {
-            
-        List<Racer> racer = new ArrayList<>();
-//        String firstRacer = print.racersString(fileData.getRacer()).substring(0, print.racersString(fileData.getRacer()).indexOf("\n"));
-        String racerInTop = "1. Esteban Ocon\t\t| FORCE INDIA MERCEDES\t| 54:13.028";
-//        assertEquals(racerInTop, firstRacer);
-        fileData.getRacer();
-        when(fileData.getRacer()).thenReturn(racer);
-        verify(fileData, atLeastOnce()).getRacer();
+
+        Painter print = new Painter();
+        when(fileReader.readFile(anyString())).thenReturn("EOF_Esteban Ocon_FORCE INDIA MERCEDES").thenReturn("EOF2018-05-24_12:17:58.810")
+                .thenReturn("EOF2018-05-24_12:12:11.838");
+        assertEquals(print.racersString(fileData.getRacer()), "FORCE INDIA MERCEDES");
+
     }
 }
