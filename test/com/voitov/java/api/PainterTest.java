3d6f31b6faf59752495ac63e1209076610dcaf90
Igor98 <igor.v.mou@gmail.com>
2019-04-05 22:42:19 +0300

    change ArrayList to singletonList

--- test/com/voitov/java/api/PainterTest.java
+++ test/com/voitov/java/api/PainterTest.java
@@ -2,7 +2,7 @@ package com.voitov.java.api;
 
 import org.junit.Test;
 import java.time.Duration;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 
@@ -12,8 +12,7 @@ public class PainterTest {
     @Test
     public void checkPrintStringForRacer() {
         Painter print = new Painter();
-        List <Racer> racers = new ArrayList<>();
-        racers.add(new Racer("Esteban Ocon", "FORCE INDIA MERCEDES", Duration.ofMillis(70028)));
+        List <Racer> racers = Collections.singletonList(new Racer("Esteban Ocon", "FORCE INDIA MERCEDES", Duration.ofMillis(70028)));
         assertEquals(print.racersString(racers), "1. Esteban Ocon\t\t| FORCE INDIA MERCEDES\t| 01:10.028\n");
 
     }
