9011c0a92e0a140a6dc31106b9e19b110370d61e
Igor Voitov <igor.v.mou@gmail.com>
2019-05-08 23:02:44 +0300

    add testing changes_1

--- src/com/voitov/java/api/Main.java
+++ src/com/voitov/java/api/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         RacerBuilder file = new RacerBuilder();
         System.out.println(new Painter().racersString(file.getRacer()));
-        //some changes
     }
 }
